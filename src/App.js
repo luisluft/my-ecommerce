ddd8d5247ebab05e74232c3e8f56b89f7545efdb
luisluft <luberluft@gmail.com>
2020-12-14 17:37:31 -0400

    add react router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
-
+import { Switch, Route } from "react-router-dom";
 import Homepage from "./pages/homepage/homepage-component";
 
 function App() {
   return (
-    <div>
-      <Homepage />
-    </div>
+    <Switch>
+      <div>
+        <Route exact path="/" component={Homepage} />
+      </div>
+    </Switch>
   );
 }
 
