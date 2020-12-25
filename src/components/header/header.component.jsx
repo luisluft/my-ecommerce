ea5a4943e1c6b78344963a933869850f20041254
luisluft <luberluft@gmail.com>
2020-12-25 18:23:20 -0400

    add cart icon

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 import { auth } from "../../firebase/firebase.utils";
 import { connect } from "react-redux";
-
+import CartIcon from "../cart-icon/cart-icon.component";
 const Header = ({ currentUser }) => (
   <div className="header">
     <Link className="logo-container" to="/">
@@ -27,6 +27,7 @@ const Header = ({ currentUser }) => (
           SIGNIN
         </Link>
       )}
+      <CartIcon />
     </div>
   </div>
 );
