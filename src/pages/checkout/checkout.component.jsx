702c8f63889e49ef7d10f2af2371a4ad94e70bae
luisluft <luberluft@gmail.com>
2021-01-02 18:07:36 -0400

    add redux to checkout page

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -1,6 +1,31 @@
 import React from "react";
 import "./checkout.styles.scss";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+import {
+  selectCartItems,
+  selectCartTotal,
+} from "../../redux/cart/cart.selectors";
 
-const CheckoutPage = () => <div>CHECKOUT PAGE</div>;
+const CheckoutPage = ({ cartItems, cartTotal }) => (
+  <div className="checkout-page">
+    <div className="checkout-header">
+      <div className="header-block">Product</div>
+      <div className="header-block">Description</div>
+      <div className="header-block">Quantity</div>
+      <div className="header-block">Price</div>
+      <div className="header-block">Remove</div>
+    </div>
+    {cartItems.map((cartItem) => cartItem.name)}
+    <div className="total">
+      <span>TOTAL: ${cartTotal}</span>
+    </div>
+  </div>
+);
 
-export default CheckoutPage;
+const mapStateToProps = createStructuredSelector({
+  cartItems: selectCartItems,
+  cartTotal: selectCartTotal,
+});
+
+export default connect(mapStateToProps)(CheckoutPage);
