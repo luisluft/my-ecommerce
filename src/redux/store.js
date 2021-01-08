d10073f5fb186263dca66e5132c6f0ebd0e36051
luisluft <luberluft@gmail.com>
2021-01-07 23:07:45 -0400

    implement redux storage persistor

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,11 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import logger from "redux-logger";
-
+import { persistStore } from "redux-persist";
 import rootReducer from "./root.reducer";
 
 const middlewares = [logger];
 
-const store = createStore(
+export const store = createStore(
   rootReducer,
   compose(
     applyMiddleware(...middlewares),
@@ -13,4 +13,4 @@ const store = createStore(
   )
 );
 
-export default store;
+export const persistor = persistStore(store);
