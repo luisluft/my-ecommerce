5005471488f592eeb7661bf7ee88e6153524af59
luisluft <luberluft@gmail.com>
2021-01-29 19:57:38 -0400

    remove logger middleware

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,4 @@
 import { createStore, applyMiddleware, compose } from "redux";
-import logger from "redux-logger";
 import { persistStore } from "redux-persist";
 import rootReducer from "./root.reducer";
 import createSagaMiddleware from "redux-saga";
@@ -8,9 +7,9 @@ import { fetchCollectionsStart } from "./shop/shop.sagas";
 const middlewares = [];
 
 const sagaMiddleware = createSagaMiddleware();
+
 middlewares.push(sagaMiddleware);
 
-if (process.env.NODE_ENV === "development") middlewares.push(logger);
 export const store = createStore(
   rootReducer,
   compose(
