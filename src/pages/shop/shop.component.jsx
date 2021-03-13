768472cb2206d97e85691f04001901e727d102ea
luisluft <luberluft@gmail.com>
2021-03-13 14:12:43 -0400

    conver shop component to use react hooks

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,32 +1,29 @@
-import { fetchCollectionsStart } from "../../redux/shop/shop.actions";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
-import React from "react";
+import { fetchCollectionsStart } from "../../redux/shop/shop.actions";
 import { Route } from "react-router-dom";
 import CollectionsOverviewContainer from "../../components/collections-overview/collections-overview.container";
 import CollectionsPageContainer from "../../pages/collection/collection.container";
-class ShopPage extends React.Component {
-  componentDidMount() {
-    const { fetchCollectionsStart } = this.props;
-    fetchCollectionsStart();
-  }
 
-  render() {
-    const { match } = this.props;
-    return (
-      <div className="shop-page">
-        <Route
-          exact
-          path={`${match.path}`}
-          component={CollectionsOverviewContainer}
-        />
-        <Route
-          path={`${match.path}/:collectionId`}
-          component={CollectionsPageContainer}
-        />
-      </div>
-    );
-  }
-}
+const ShopPage = ({ fetchCollectionsStart, match }) => {
+  useEffect(() => {
+    fetchCollectionsStart();
+  }, [fetchCollectionsStart]);
+
+  return (
+    <div className="shop-page">
+      <Route
+        exact
+        path={`${match.path}`}
+        component={CollectionsOverviewContainer}
+      />
+      <Route
+        path={`${match.path}/:collectionId`}
+        component={CollectionsPageContainer}
+      />
+    </div>
+  );
+};
 
 const mapDispatchToProps = (dispatch) => ({
   fetchCollectionsStart: () => dispatch(fetchCollectionsStart()),
